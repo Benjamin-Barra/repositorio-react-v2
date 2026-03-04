74051360090a1436a427b81f85a8f203e513821d
El-Benyi <barradiazbenjamin@gmail.com>
2026-03-04 02:09:54 -0300

    Esqueleto de la pagina avanzando

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,9 +1,32 @@
+import "./Skills.css"
+
 export default function Skills(){
     
+
     
     return (
         <section className="skills">
-            <h2>Aquí van a estar las Skills cards</h2>
+            <div className="container_icons">
+                <span className="icono_skill">
+                    <ion-icon name="logo-python"></ion-icon>
+                </span>
+
+                <span className="icono_skill">
+                    <ion-icon name="logo-html5"></ion-icon>
+                </span>
+
+                <span className="icono_skill">
+                    <ion-icon name="logo-css3"></ion-icon>
+                </span>
+
+                <span className="icono_skill">
+                    <ion-icon name="logo-javascript"></ion-icon>
+                </span>
+
+                <span className="icono_skill">
+                    <ion-icon name="logo-react"></ion-icon>
+                </span>
+            </div>
         </section>
     )
 }
--- src/components/Trabajos.jsx
+++ src/components/Trabajos.jsx
@@ -1,14 +1,56 @@
+import { SiFlask, SiMysql } from "react-icons/si"
+import finanzasImg from "../assets/Finanzas.png"
+import marcaje1 from "../assets/marcaje.png"
+import marcaje2 from "../assets/marcaje2.png"
+import { FaCss3Alt, FaHtml5, FaJsSquare, FaPython } from "react-icons/fa"
+import "./Trabajos.css"
+
 export default function Trabajos(){
     
-    const works = {
-        id: 1,
-        nombre: "SudFinanzas",
-        desc: ""
+    const works = [
+        {
+            id: 1,
+            nombre: "SudFinanzas",
+            desc: 'Aplicación web desarrollada para la empresa SudCap durante mi período de práctica profesional. Su función principal es la gestión y control de activos, junto con el registro y seguimiento del flujo monetario de la empresa.',
+            img: [finanzasImg],
+            tec: ["flask", "python", "html", "css", "javascript", "mysql"]
+        },
+        {
+            id: 2,
+            nombre: "SudMarcaje",
+            desc: "Aplicación web desarrollada para la empresa SudCap durante mi período de práctica profesional. Permite iniciar y gestionar un contador para registrar las horas de trabajo de los empleados, además de obtener su ubicación en tiempo real y visualizarla en un mapa accesible únicamente para administradores.",
+            img: [marcaje1,marcaje2],
+            tec: ["flask", "python", "html", "css", "javascript", "mysql"]
+        }
+    ]
+
+    const tecIconos = {
+        flask: <SiFlask />,
+        python: <FaPython />,
+        html: <FaHtml5 />,
+        css: <FaCss3Alt />,
+        javascript: <FaJsSquare/>,
+        mysql: <SiMysql />
     }
     
     return (
         <section className="container_trabajos">
-            {works}
+            {works.map(work => (
+                <div className="trabajo" key={work.id}>
+                    <h1>{work.nombre}</h1>
+                    <p>{work.desc}</p>
+                    {work.img.map((imagen, index) => (
+                        <img key={index} src={imagen} alt={work.nombre} />
+                    ))}
+                    <div className="tec-icons">
+                        {work.tec.map((tec, index) => (
+                            <span key={index}>
+                            {tecIconos[tec]}
+                            </span>
+                        ))}
+                    </div>
+                </div>
+            ))}
         </section>
     )
 }
